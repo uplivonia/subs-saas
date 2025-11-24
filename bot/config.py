1981b900229109df381f33458fd249280187c5c9
Staffeya <rutkevicsviktors@gmail.com>
2025-11-24 09:29:02 +0200

    Use Render backend URL

--- bot/config.py
+++ bot/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings
 
 class BotSettings(BaseSettings):
     BOT_TOKEN: str = "8350395273:AAEFuqUZi7Gpaq1MCzM2Cn3HbmguI37lECg"
-    BACKEND_URL: str = "http://127.0.0.1:8000"
+    BACKEND_URL: str = "https://subs-saas.onrender.com"
     DEFAULT_LANGUAGE: str = "en"
 
     class Config:
